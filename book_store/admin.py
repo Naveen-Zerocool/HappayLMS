be9c27b73d2d7c7cbe555538fa075f5712b08927
Naveen <ilovemybusiness.square@gmail.com>
2022-05-24 23:58:31 +0530

    Admin changes

--- book_store/admin.py
+++ book_store/admin.py
@@ -1,12 +1,37 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 from django.contrib.auth.models import Group
 
 from book_store.models import Category, Author, Book
 
 
-admin.site.register(Category)
-admin.site.register(Author)
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'author__name', 'category__title', 'publisher_name']
+    list_display = ['title', 'author', 'category']
+    list_filter = ['category__name']
+    date_hierarchy = 'published_date'
+    autocomplete_fields = ['author', 'category']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name']
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
 
 
 admin.site.unregister(Group)
+
+
+AdminSite.site_header = "Happay LMS Admin"
+AdminSite.site_title = "Happay LMS Admin"
+AdminSite.site_url = None
+AdminSite.index_title = "Happay LMS Administration"
