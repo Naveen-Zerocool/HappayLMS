dceeb13f9aa3b378f263356acd094af2bfe50209
Naveen <ilovemybusiness.square@gmail.com>
2022-05-25 02:34:52 +0530

    Added a basic swagger doc

--- HappayLMS/urls.py
+++ HappayLMS/urls.py
@@ -15,10 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
 from book_store import api_urls
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Happay LMS API",
+        default_version='v1',
+        description="Happay LMI API can be used for multiple operations like adding a author,"
+                    "book, category, searching, etc. All available API documentation are given below",
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(api_urls)),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
